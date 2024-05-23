c400a9400d750bc50a26f258de8e9afa9050dd1b
mvysotskyi <vysotskyi.pn@ucu.edu.ua>
2024-05-23 21:25:43 +0300

    use localStorage

--- front-end/src/components/NormalTraining.tsx
+++ front-end/src/components/NormalTraining.tsx
@@ -5,11 +5,17 @@ import DaySelector from './DaySelector';
 import ExerciseForm from './ExerciseForm';
 import ExerciseResults from './ExerciseResults';
 
-const NormalTraining = () => {
+function NormalTraining() {
   const [trainings, setTrainings] = useState();
   const [selectedDay, setSelectedDay] = useState('');
-  const [inputData, setInputData] = useState({});
-  const [setSelection, setSetSelection] = useState({});
+  const [inputData, setInputData] = useState(() => {
+    const savedData = localStorage.getItem('inputData');
+    return savedData ? JSON.parse(savedData) : {};
+  });
+  const [setSelection, setSetSelection] = useState(() => {
+    const savedSelection = localStorage.getItem('setSelection');
+    return savedSelection ? JSON.parse(savedSelection) : {};
+  });
 
   useEffect(() => {
     const handleGetTrainings = async () => {
@@ -28,33 +34,43 @@ const NormalTraining = () => {
       const newData = { ...prevState };
       if (!newData[day]) newData[day] = {};
       newData[day][exerciseIndex] = Array.from({ length: sets }).map(() => ({ weight: '', reps: '' }));
+      localStorage.setItem('inputData', JSON.stringify(newData));
       return newData;
     });
-    setSetSelection((prevState) => ({
-      ...prevState,
-      [day]: {
-        ...prevState[day],
-        [exerciseIndex]: 0,
-      },
-    }));
+    setSetSelection((prevState) => {
+      const newSelection = {
+        ...prevState,
+        [day]: {
+          ...prevState[day],
+          [exerciseIndex]: 0,
+        },
+      };
+      localStorage.setItem('setSelection', JSON.stringify(newSelection));
+      return newSelection;
+    });
   };
 
   const handleInputChange = (day, exerciseIndex, setIndex, field, value) => {
     setInputData((prevState) => {
       const newData = { ...prevState };
       newData[day][exerciseIndex][setIndex][field] = value;
+      localStorage.setItem('inputData', JSON.stringify(newData));
       return newData;
     });
   };
 
   const handleSetSelectionChange = (day, exerciseIndex, event) => {
-    setSetSelection((prevState) => ({
-      ...prevState,
-      [day]: {
-        ...prevState[day],
-        [exerciseIndex]: event.target.value,
-      },
-    }));
+    setSetSelection((prevState) => {
+      const newSelection = {
+        ...prevState,
+        [day]: {
+          ...prevState[day],
+          [exerciseIndex]: event.target.value,
+        },
+      };
+      localStorage.setItem('setSelection', JSON.stringify(newSelection));
+      return newSelection;
+    });
   };
 
   return (
