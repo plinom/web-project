6b1495be1fa465080804193aec38aea268b5960c
mvysotskyi <vysotskyi.pn@ucu.edu.ua>
2024-05-23 23:43:39 +0300

    have bug yet

--- front-end/src/components/NormalTraining.tsx
+++ front-end/src/components/NormalTraining.tsx
@@ -5,7 +5,7 @@ import DaySelector from './DaySelector';
 import ExerciseForm from './ExerciseForm';
 import ExerciseResults from './ExerciseResults';
 
-function NormalTraining() {
+const NormalTraining = () => {
   const [trainings, setTrainings] = useState();
   const [selectedDay, setSelectedDay] = useState('');
   const [inputData, setInputData] = useState(() => {
@@ -16,6 +16,12 @@ function NormalTraining() {
     const savedSelection = localStorage.getItem('setSelection');
     return savedSelection ? JSON.parse(savedSelection) : {};
   });
+  const [results, setResults] = useState(() => {
+    const savedResults = localStorage.getItem('results');
+    return savedResults ? JSON.parse(savedResults) : {};
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState("");
 
   useEffect(() => {
     const handleGetTrainings = async () => {
@@ -27,6 +33,8 @@ function NormalTraining() {
 
   const handleSelectChange = (event) => {
     setSelectedDay(event.target.value);
+    setIsEditing(false);
+    setEditId("");
   };
 
   const handleSetNumberChange = (day, exerciseIndex, sets) => {
@@ -34,43 +42,87 @@ function NormalTraining() {
       const newData = { ...prevState };
       if (!newData[day]) newData[day] = {};
       newData[day][exerciseIndex] = Array.from({ length: sets }).map(() => ({ weight: '', reps: '' }));
-      localStorage.setItem('inputData', JSON.stringify(newData));
       return newData;
     });
-    setSetSelection((prevState) => {
-      const newSelection = {
-        ...prevState,
-        [day]: {
-          ...prevState[day],
-          [exerciseIndex]: 0,
-        },
-      };
-      localStorage.setItem('setSelection', JSON.stringify(newSelection));
-      return newSelection;
-    });
+    setSetSelection((prevState) => ({
+      ...prevState,
+      [day]: {
+        ...prevState[day],
+        [exerciseIndex]: 0,
+      },
+    }));
   };
 
   const handleInputChange = (day, exerciseIndex, setIndex, field, value) => {
     setInputData((prevState) => {
       const newData = { ...prevState };
       newData[day][exerciseIndex][setIndex][field] = value;
-      localStorage.setItem('inputData', JSON.stringify(newData));
       return newData;
     });
   };
 
   const handleSetSelectionChange = (day, exerciseIndex, event) => {
+    setSetSelection((prevState) => ({
+      ...prevState,
+      [day]: {
+        ...prevState[day],
+        [exerciseIndex]: event.target.value,
+      },
+    }));
+  };
+
+  const handleSaveResult = () => {
+    const timestamp = new Date().toISOString();
+    const uniqueId : string = isEditing ? editId : Date.now().toString();
+    setResults((prevState) => {
+      const newResults = { ...prevState };
+      if (!newResults[selectedDay]) newResults[selectedDay] = {};
+      Object.keys(inputData[selectedDay]).forEach((exerciseIndex) => {
+        if (!newResults[selectedDay][uniqueId]) {
+          newResults[selectedDay][uniqueId] = {};
+        }
+        const setIndex = setSelection[selectedDay][exerciseIndex];
+        if (!newResults[selectedDay][uniqueId][exerciseIndex]) {
+          newResults[selectedDay][uniqueId][exerciseIndex] = [];
+        }
+        newResults[selectedDay][uniqueId][exerciseIndex] = inputData[selectedDay][exerciseIndex];
+      });
+      localStorage.setItem('results', JSON.stringify(newResults));
+      return newResults;
+    });
+    setIsEditing(false);
+    setEditId("");
+  };
+
+  const handleDeleteResult = (day, uniqueId) => {
+    setResults((prevState) => {
+      const newResults = { ...prevState };
+      delete newResults[day][uniqueId];
+      if (Object.keys(newResults[day]).length === 0) {
+        delete newResults[day];
+      }
+      localStorage.setItem('results', JSON.stringify(newResults));
+      return newResults;
+    });
+  };
+
+  const handleEditResult = (day, uniqueId) => {
+    setSelectedDay(day);
+    setInputData((prevState) => {
+      const newData = { ...prevState };
+      newData[day] = results[day][uniqueId];
+      return newData;
+    });
     setSetSelection((prevState) => {
-      const newSelection = {
-        ...prevState,
-        [day]: {
-          ...prevState[day],
-          [exerciseIndex]: event.target.value,
-        },
-      };
-      localStorage.setItem('setSelection', JSON.stringify(newSelection));
+      const newSelection = { ...prevState };
+      Object.keys(results[day][uniqueId]).forEach((exerciseIndex) => {
+        newSelection[day] = newSelection[day] || {};
+        newSelection[day][exerciseIndex] = 0;
+      });
       return newSelection;
     });
+    setIsEditing(true);
+    setEditId(uniqueId);
   };
 
   return (
@@ -85,9 +137,16 @@ function NormalTraining() {
           handleSetNumberChange={handleSetNumberChange}
           handleInputChange={handleInputChange}
           handleSetSelectionChange={handleSetSelectionChange}
+          handleSaveResult={handleSaveResult}
+          isEditing={isEditing}
         />
       )}
-      <ExerciseResults inputData={inputData} data={data} />
+      <ExerciseResults
+        results={results}
+        data={data}
+        handleDeleteResult={handleDeleteResult}
+        handleEditResult={handleEditResult}
+      />
     </div>
   );
 }
